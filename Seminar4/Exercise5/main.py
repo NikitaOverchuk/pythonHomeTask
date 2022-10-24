31a27e5f36a66449bc6ce024e210bce7151e131e
NikitaOverchuk <108486721+NikitaOverchuk@users.noreply.github.com>
2022-10-24 19:19:34 +0300

    the right version

--- Seminar4/Exercise5/main.py
+++ Seminar4/Exercise5/main.py
@@ -1,41 +1,27 @@
-fin1 = open("Seminar4\Exercise5\input.txt", encoding="utf8") 
-fin2 = open("Seminar4\Exercise5\input1.txt", encoding="utf8")
-fout = open("Seminar4\Exercise5\output.txt", "w", encoding="utf8")
-m1, m2 = list(fin1.readline().strip().split()), list(fin2.readline().strip().split())
-my_dict = dict()
-for i in range(0, len(m1), 2):                        #заполняем словарь первым многочленом
-    num = list(m1[i].split("x^"))
-    if len(num) == 1:                                   #проверка на х^0 и х^1
-        if num[0].isdigit():
-            num.append(0)
-        else:
-            num = num[0].split("x")
-    value, n = num
-    if value == '':
-        value = 1
-    if n == '':
-        n = 1
-    value, n = int(value), int(n)
-    if i != 0 and m1[i - 1] == '-':
-        value = -value
-    my_dict[n] = my_dict.get(n, 0) + value
+def fill_dict(m):
+    for i in range(0, len(m), 2):                   # заполняем словарь многочленами
+        num = list(m[i].split("x^"))
+        if len(num) == 1:                              # проверка на х^0 и х^1
+            if num[0].isdigit():
+                num.append(0)
+            else:
+                num = num[0].split("x")
+        value, n = num
+        if value == '':
+            value = 1
+        if n == '':
+            n = 1
+        value, n = int(value), int(n)
+        if i != 0 and m[i - 1] == '-':
+            value = -value
+        my_dict[n] = my_dict.get(n, 0) + value
 
-for i in range(0, len(m2), 2):                #заполняем словаpь вторым многочленом
-    num = list(m2[i].split("x^"))
-    if len(num) == 1:
-        if num[0].isdigit():
-            num.append(0)
-        else:
-            num = num[0].split("x")
-    value, n = num
-    if value == '':
-        value = 1
-    if n == '':
-        n = 1
-    value, n = int(value), int(n)
-    if i != 0 and m2[i - 1] == '-':
-        value = -value
-    my_dict[n] = my_dict.get(n, 0) + value
+
+fin1, fin2 = open("input.txt", encoding="utf8"), open("input1.txt", encoding="utf8")
+fout = open("output.txt", "a", encoding="utf8")
+mnog1, mnog2, my_dict = list(fin1.readline().strip().split()), list(fin2.readline().strip().split()), dict()
+fill_dict(mnog1)
+fill_dict(mnog2)
 lst = list(my_dict.items())
 lst.sort(reverse=True)
 print(lst[0][1], "x^", lst[0][0], sep="", end=" ", file=fout)          # выводим первый одночлен, чтобы +\- в начале писать
